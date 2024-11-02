9854744eacab553375cc3f36cbd10eeba81cea90
Fares Muhammad <eng-fares.muhammad1823@alexu.edu.eg>
2024-11-02 22:54:52 +0200

    Handle Record Deletion

--- app/Http/Controllers/API/V1/RecordController.php
+++ app/Http/Controllers/API/V1/RecordController.php
@@ -15,9 +15,11 @@ use App\Models\Currency;
 use App\Models\Record;
 use App\Models\Transfer;
 use App\Models\Wallet;
+use App\Services\DeleteRecordService;
 use App\Services\EditRecord;
 use App\Services\EditTransfer;
 use App\Services\NewRecord;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -67,61 +69,13 @@ class RecordController extends Controller
     }
 
 
-    public function delete(Wallet $wallet, Record $record)
+    public function delete(Record $record, DeleteRecordService $service): JsonResponse
     {
-        return DB::transaction(function () use ($record, $wallet) {
-            switch ($record->type) {
-                case ('Expense'):
-                    $record->balance->update(['value' => $record->balance->value + $record->amount]);
-                    BalancePerDate::updateOrCreate(
-                        [
-                            'date' => $record->date,
-                            'wallet_id' => $wallet->id,
-                            'balance_id' => $record->balance->id
-                        ],
-                        ['value' => $record->balance->value]
-                    );
-                    $record->delete();
-                    break;
-                case ('Income'):
-                    $record->balance->update(['value' => $record->balance->value - $record->amount]);
-                    BalancePerDate::updateOrCreate(
-                        [
-                            'date' => $record->date,
-                            'wallet_id' => $wallet->id,
-                            'balance_id' => $record->balance->id
-                        ],
-                        ['value' => $record->balance->value]
-                    );
-                    $record->delete();
-                    break;
-                default:
-                    $senderBalance = Balance::find($record->transfer->sender_balance);
-                    $receiverBalance = Balance::find($record->transfer->receiver_balance);
-
-                    $senderBalance->update(['value' => $senderBalance->value + $record->amount]);
-                    $receiverBalance->update(['value' => $receiverBalance->value - $record->amount]);
-
-                    BalancePerDate::updateOrCreate(
-                        [
-                            'date' => $record->date,
-                            'wallet_id' => $record->transfer->sender_wallet,
-                            'balance_id' => $senderBalance->id
-                        ],
-                        ['value' => $senderBalance->value]
-                    );
-                    BalancePerDate::updateOrCreate(
-                        [
-                            'date' => $record->date,
-                            'wallet_id' => $record->transfer->receiver_wallet,
-                            'balance_id' => $receiverBalance->id
-                        ],
-                        ['value' => $receiverBalance->value]
-                    );
-
-                    $record->delete();
-            }
-            return redirect()->route('records', ['wallet' => $wallet->id])->with('message', 'Record deleted successfully');
-        });
+        return match ($record->type) {
+            RecordType::Expense => $service->deleteExpenseRecord($record),
+            RecordType::Income => $service->deleteIncomeRecord($record),
+            RecordType::Transfer => $service->deleteTransferRecord($record),
+            default => throw new HttpResponseException(apiResponse("Unknown Error with the record type", status: 400)),
+        };
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -37,6 +37,7 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::post('/transfer', 'transfer');
         Route::put('/update-record/{record}', 'updateRecord');
         Route::put('/update-transfer/{record}', 'updateTransfer');
+        Route::delete('/delete-record/{record}', 'delete');
     });
 });
 
