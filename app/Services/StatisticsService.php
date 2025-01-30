e3a10bb4262114d879f1e61c0c771c0c646b82aa
Fares Muhammad <eng-fares.muhammad1823@alexu.edu.eg>
2025-01-30 02:16:09 +0200

    Integrate rate exchange and cache the result

--- app/Services/StatisticsService.php
+++ app/Services/StatisticsService.php
@@ -2,38 +2,54 @@
 
 namespace App\Services;
 
-use Worksome\Exchange\Facades\Exchange;
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
 
 class StatisticsService
 {
 
-    public function totalBalance()
+    public function totalBalance(): array
     {
         $wallets = auth()->user()->wallets;
         $totalBalanceInEGP = 0;
         $balances = [];
-        $rate = 50.3;//todo integrate with third party
         foreach ($wallets as $wallet) {
             if (isset($balances[$wallet->currency]))
                 $balances[$wallet->currency] += $wallet->balance;
             else
                 $balances[$wallet->currency] = $wallet->balance;
 
-            if ($wallet->currency != 'EGP')
-            {
+            if ($wallet->currency != 'EGP') {
+                $rate = $this->rate(from: $wallet->currency);
                 $totalBalanceInEGP += $wallet->balance * $rate;
                 $balances[$wallet->currency] = [
                     'currency' => $wallet->currency,
                     'balance' => $balances[$wallet->currency],
+                    'rate' => $rate,
                     'EGPBalance' => $balances[$wallet->currency] * $rate,
                 ];
-            }
-            else
+            } else
                 $totalBalanceInEGP += $wallet->balance;
         }
         return [
             'totalBalanceInEGP' => $totalBalanceInEGP,
             'balances' => $balances,
         ];
+    }
+
+    private function rate($from = 'USD', $to = 'EGP'): float
+    {
+        if (Cache::has("currency-{$from}-{$to}")) {
+            return Cache::get("currency-{$from}-{$to}");
+        }
+        $response = Http::withHeader('apy-token', 'APY0GhvlsJRhxNesUgcfk8BnjmgvIN1X2wT40phVpIoDf3XegXngr4wj7MZiBAYHd')
+            ->post("https://api.apyhub.com/data/convert/currency", [
+                'source' => $from,
+                'target' => $to,
+            ]);
+        $rate = round($response->json('data'), 2);
+        Cache::put("currency-{$from}-{$to}", $rate, 60 * 24);
+        return $rate;
     }
 }
